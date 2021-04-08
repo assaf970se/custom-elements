8725dfaf15f06672c2f1211f2c68c5f3d2e01982
assaf970se <assafse@wix.com>
2021-04-08 16:02:36 +0300

    Update wa-nav-bar.js

--- nav-bar/wa-nav-bar.js
+++ nav-bar/wa-nav-bar.js
@@ -626,18 +626,18 @@ class WaNavBar extends HTMLElement {
     }
 
     async trackClickEvent(redirectUrl = '') {
-        const obj = {
-            uniqueId: 'customEl',
-            label: 'customEl',
-            name: 'customEl',
-            type: 'customEl',
-            linkTo: redirectUrl,
-            url: `${location.href}`,
-        };
-        const route = `https://app.wixanswers.com/api/bi/website-click?uniqueId=${obj.uniqueId}&type=${obj.type}&name=${obj.name}&url=${obj.url}&label=${obj.label}&linkTo=${obj.linkTo}`;
-        const res = await fetch(route);
-        const resText = await res.text();
-        console.log(resText);
+        // const obj = {
+        //     uniqueId: 'customEl',
+        //     label: 'customEl',
+        //     name: 'customEl',
+        //     type: 'customEl',
+        //     linkTo: redirectUrl,
+        //     url: `${location.href}`,
+        // };
+        // const route = `https://app.wixanswers.com/api/bi/website-click?uniqueId=${obj.uniqueId}&type=${obj.type}&name=${obj.name}&url=${obj.url}&label=${obj.label}&linkTo=${obj.linkTo}`;
+        // const res = await fetch(route);
+        // const resText = await res.text();
+        // console.log(resText);
         if (redirectUrl) window.location.href = redirectUrl;
     }
 }
